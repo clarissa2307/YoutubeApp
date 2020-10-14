41115a9221be193720275ccdfed4508064a6ea2b
clarissa2307 <marthaleo07@gmail.com>
2020-10-13 21:58:56 -0500

    mostrar videos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,13 +9,44 @@ import { YoutubeService } from '../../services/youtube.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor( private YoutubeService: YoutubeService) { }
+  videos: Video[] = [];
 
-  ngOnInit(){
-   this.YoutubeService.getVideos()
-       .subscribe( resp => {
-         console.log( resp );
-       })
+  constructor(private youtubeService: YoutubeService) { }
+
+  ngOnInit(): void {
+    this.cargarVideos();
   }
 
+  cargarVideos() {
+
+    this.youtubeService.getVideos()
+    .subscribe( resp => {
+
+      this.videos.push( ...resp );
+      console.log(this.videos);
+
+    });
+  }
+
+  mostrarVideo( video: Video ) {
+
+    Swal.fire({
+      html: `
+      <h4> ${ video.title } </h4>
+      <hr>
+      <iframe width="100%"
+        height="315"
+        src="https://www.youtube.com/embed/${ video.resourceId.videoId }"
+        frameborder="0"
+        allow="accelerometer;
+        autoplay;
+        clipboard-write;
+        encrypted-media;
+        gyroscope;
+        picture-in-picture"
+        allowfullscreen>
+      </iframe>
+      `
+    });
+  }
 }
