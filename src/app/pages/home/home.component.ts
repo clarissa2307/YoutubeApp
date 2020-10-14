d46d869175581e5d28bf02c23850dd86abd38ff5
clarissa2307 <marthaleo07@gmail.com>
2020-10-13 22:38:46 -0500

    mas videos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,7 +10,7 @@ import Swal from 'sweetalert2'
 })
 export class HomeComponent implements OnInit {
 
-  videos: Video[] = [];
+  videos: video[] = [];
 
   constructor(private youtubeService: YoutubeService) { }
 
